b775cd4e6ca201489e418f08190638afbc2f7175
Angel Thompson <n00191093@student.iadt.ie>
2022-02-27 21:33:22 +0000

    reuirments finished

--- myRoster/app/Http/Controllers/Controller.php
+++ myRoster/app/Http/Controllers/Controller.php
@@ -8,6 +8,10 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
+
+
 {
+        // a controller contains the logic, they load the correct view of your webpage
+    // i am managing the display of each view with customised controllers with the relevant view
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
