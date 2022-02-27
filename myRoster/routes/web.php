65d65e14d81ff4826300acaa70c704b5976c677b
Angel Thompson <n00191093@student.iadt.ie>
2022-02-27 15:41:26 +0000

    Controllers

--- myRoster/routes/web.php
+++ myRoster/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,15 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('about', 'App\Http\Controllers\AboutController@about');
+
+Route::get('/login', [LoginController::class, 'login']);
+
 Route::get('/register', function () {
     return view('register');
 });
 Route::get('/myRoster', function () {
-    return view('my Roster');
+    return view('myRoster');
 });
